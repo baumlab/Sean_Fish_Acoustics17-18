7557a69763bcff2dc79ab8f1f005f68ab6eebc3b
Thor <sdimoff@hawaii.edu>
2019-05-14 09:01:39 -0700

    Added all new ACI and SPL data as well as created csv's for the independent sites in 2018.  Started a new r markdown file to create graphs of the ACI and SPL for study.

---
+++ R_Scripts/SD_Scripts/2018_ExploratoryPlots.R
@@ -0,0 +1,9 @@
+#load required packages
+require(ggplot2)
+require(plyr)
+library(dplyr)
+require(stringr)
+require(reshape2)
+
+#read in data
+site5.18 <- read.csv("Raw_Data/SPL.ACI_2018_MP_5.csv")
